867873bd5d4d68a8b15236431f911b7b9c7eaf1b
“Reese <reesegreen2014@me.com>
2024-01-13 12:01:21 -0700

    Initial commit

--- comparisons.js
+++ comparisons.js
@@ -18,12 +18,15 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
 // this should log: false
+console.log("is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers)
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
+console.log("Is numberTeachers not equal to numberStudents", numberTeachers !== numberStudents)
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
+console.log("is numberStudents greater than or equal to 21?", numberStudents >= 21)
 
 
 // #-------------------
@@ -36,12 +39,13 @@ var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+// This line of code is comparing whether the number of friends is greater than the number of siblings. The expected return is true, as 6 is greater than 2. 
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+// This line of code is comparing whether the number of attendees is not strictly equal to the number of meals. The expected output is true, as the two values are not strictly equal.
 
 // #-------------------
 // PART 3: Logical Operators
@@ -63,10 +67,17 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
+console.log("Does the dog love to play and loves treats?", lovesToPlay && lovesTreats) 
+// return would be true, as dog loves to play and eat treats
+
 
 // YOU DO:
 // Determine if the dog loves to play or loves the dog park
+console.log ("Does the dog love to play or love the park?", lovesToPlay || lovesDogPark)
+// return would be true, as dog loves to play but does not like dog park, however the Or operator only cares that one is true.
 
 // YOU DO:
 // Determine if the dog loves to play and is a puppy
 // HINT: Use the age variable and assume that a puppy is less than 2 years old
+console.log("Does the dog love to play and is the dog a puppy?", lovesToPlay && age < 2)
+// return would be true, as dog loves to play and is less than 2 years old.
